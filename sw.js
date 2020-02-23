de1aca23fc3a1eafb4dcdb99c20e83117f7f26f4
Aramos <entrellardat.developer@gmail.com>
2020-02-23 13:17:05 +0100

    service workers

---
+++ sw.js
@@ -0,0 +1,48 @@
+const VERSION = "v1";
+
+self.addEventListener('install', event => {
+    debugger;
+    console.log('entro');
+    event.waitUntil(precache())
+});
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    // get
+    if (request.method != "GET") {
+        return;
+    }
+
+    // buscar en cache 
+    event.respondWith(cachedResponse(request));
+
+    // actuqalizar el cache
+
+    event.waitUntil(updateCache(request));
+
+})
+
+async function precache() {
+    const cache = await caches.open(VERSION);
+    return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/index.js',
+        '/assets/MediaPlayer.js',
+        '/assets/plugins/AutoPlay.js',
+        '/assets/index.css',
+        '/assets/BigBuckBunny.mp4'
+    ]);
+}
+
+async function cachedResponse(request) {
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request) {
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response);
+}
