13a66814dcc4f76feadaa7e694a87e87f0ad0fed
Aramos <entrellardat.developer@gmail.com>
2020-02-25 20:41:00 +0100

    Implementacion decorator , observer y singleton

---
+++ ejercicios/decorator/index.ts
@@ -0,0 +1,54 @@
+console.log('hola');
+class Field {
+    errors : string[] ;
+    input : HTMLInputElement;
+
+    constructor(input : HTMLInputElement){
+        this.input = input;
+        this.errors = [] ;
+
+        let errorMessage = document.createElement('p');
+        errorMessage.className = 'text-danger';
+
+        this.input.parentNode.insertBefore(errorMessage,this.input.nextSibling);
+
+        this.input.addEventListener('input' , () => {
+            console.log('caca');
+            this.errors = [] ;
+            this.validate();
+            errorMessage.innerText = this.errors[0] || '';
+        });
+    }
+
+    validate(){}
+}
+
+function RequiredFieldDecorator(field : Field): Field{
+    let validate = field.validate;
+    field.validate = function() {
+        validate();
+        let value = field.input.value;
+        if(!value){
+            field.errors.push('Requerido');
+        }
+        
+    }
+    return field;
+}
+
+function EmailFieldDecorator(field : Field): Field{
+    let validate = field.validate;
+    field.validate = function() {
+        validate();
+        let value = field.input.value;
+        if(value.indexOf("@") === -1){
+            field.errors.push('Debe ser un mail');
+        }
+        
+    }
+    return field;
+}
+
+let field = new Field(document.querySelector('#email'));
+field = RequiredFieldDecorator(field);
+field = EmailFieldDecorator(field);
